1af23d786770e66e64af98118cf70d075c6ae95b
Mafra <juan.carlos2113@hotmail.com>
2026-01-31 18:49:15 -0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,42 @@
+//Seleciona os elementos do formulário
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
+
+//captura o input para formatar o valor
+amount.oninput = () => {
+  //obtém o valor atual do input e remove os caracteres não numéricos
+  let value = amount.value.replace(/\D/g, "")
+
+  //Transforma o valor em centavos
+  value = Number(value) / 100
+
+  //atualiza o valor do input
+  amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value){
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })
+
+  return value
+}
+//Captura o evento de submit do formulário para obter os valores.
+form.onsubmit = (event) => {
+  //Previne o comportamento padrão de recarregar a página.
+  event.preventDefault()
+
+  // Cria um objeto com os detalhes na nova despesa.
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  }
+}
+
